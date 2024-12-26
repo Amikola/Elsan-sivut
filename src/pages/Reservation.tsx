4e40e54be6065847caecea3bf413aa3d698b430d
Amikola <atte.ikola@gmail.com>
2024-12-26 19:07:07 +0200

    Added reservation system

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+
 function Reservation() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/2.8.3/iframeResizer.min.js';
+    script.onload = () => {
+      // @ts-ignore
+      iFrameResize({ checkOrigin: false }, '#reservationIframe60670');
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div>
-      <h1>Reservation</h1>
+      <iframe
+        width="100%"
+        frameBorder="0"
+        src="https://varaa.timma.fi/reservation/hierojaikolaelsa"
+        id="reservationIframe60670"
+      ></iframe>
     </div>
   );
 }
