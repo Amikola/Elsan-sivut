db856903170e34071f47cb479c1156be9aa42090
Amikola <atte.ikola@gmail.com>
2024-12-27 19:49:52 +0200

    Fixed issue with bottom bar

--- src/pages/BasicConditions/BasicConditions.tsx
+++ src/pages/BasicConditions/BasicConditions.tsx
@@ -2,9 +2,11 @@ import './BasicConditions.css';
 
 function BasicConditions() {
   return (
+    <div className='basic-conditions'>
     <div className="basic-conditions-container">
       <h1>Peruutusehdot</h1>
       <p>Peruutuksen on tapahduttava viimeistään 24h ennen varattua aikaa. Saman vuorokauden aikana peruutetuista sekä peruuttamattomista ajoista yrityksellä on oikeus periä 50% palvelun hinnasta.</p>
+    </div>
     </div>
   );
 }
