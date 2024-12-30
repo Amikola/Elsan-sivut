1bb41224f0941c5f390ab5d6c1fbd10e5a0016e4
Atte <atte.ikola@gmail.com>
2024-12-30 19:19:03 +0200

    Added homepage for computer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,43 @@
 import { NavLink } from "react-router-dom";
-import { InstagramEmbed } from 'react-social-media-embed';
+import {InstagramEmbed } from 'react-social-media-embed';
+import './Home.css';
 
 function Home() {
   return (
     <div>
-      <div>
+      <div className="Title">
+        <div className="Text">
         <h1>Tervetuloa hierontaan.</h1>
         <p>Meiltä saat hierontaa juuri omiin tarpeisiisi, halusit sitten rentoutua, vähentää lihaskipuja, parantaa liikkuvuutta tai palautua nopeammin.</p>
-        <NavLink to="/reservation" className="button">Varaa aika!</NavLink>     
+        <NavLink to="/reservation" className="button">Varaa aika!</NavLink>
         </div>
-      <div>
         <img src="src/assets/IMG_0418.PNG" alt="Logo"></img>
-      </div>
-      <div>
+        </div>
+      <div className="Services">
         <h1>Palvelut</h1>
         
-        <div>
+        <div className="Sports">
           <img src="src/assets/IMG_7104.jpg" alt="Kuva"></img>
           <h2>Urheiluhieronta</h2>
           <p>Urheiluhieronta sopii jokaiselle liikkujalle. Oli sitten tavoitteena palautua menneistä harjoituksista tai ylläpitää liikuntakykyä. Käytän tarvittaessa myös erilaisia erikoistekniikoita.</p>
           <NavLink to="/reservation" className="button">Varaa aika!</NavLink>  
         </div>
 
-        <div>
+        <div className="Classic">
           <img src="src/assets/IMG_7094.jpg" alt="Kuva"></img>
           <h2>Klassinen hieronta</h2>
           <p>Klassinen hieronta sopii kaikille jotka haluavat rentoutua. Hieronta tutkitusti vähentää stressiä ja rentouttaa, joten se on oiva myös toimistotyöntekijöille.</p>
           <NavLink to="/reservation" className="button">Varaa aika!</NavLink>  
         </div>
-        <blockquote data-instgrm-permalink="https://www.instagram.com/instagram/" data-instgrm-version="12"></blockquote>
-        <script async src="//www.instagram.com/embed.js"></script>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className="Socials">
+      <h1>Seuraa meitä</h1>
+      <p>Facebookissa ja Instagramissa!</p>
     <InstagramEmbed url="https://www.instagram.com/hierojaelsa?igsh=MXBmcjl1bzFmM3ZobA%3D%3D&utm_source=qr" width={800} />
+    <div className="Links">
+    <a href="https://www.facebook.com/profile.php?id=61565476470926"> Facebook</a>
+    <a href="https://www.facebook.com/profile.php?id=61565476470926"> Instagram</a>
+    </div>
     </div>
       
     </div>
