c6f83013bd3b41bb523e7c4c6a37967fa69b8be1
Atte <atte.ikola@gmail.com>
2025-01-02 14:06:12 +0200

    Added loading text

--- src/pages/GiftCard/GiftCard.tsx
+++ src/pages/GiftCard/GiftCard.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import "./GiftCard.css";
 
 function GiftCard() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/2.8.3/iframeResizer.min.js';
     script.onload = () => {
       // @ts-ignore
       iFrameResize({ checkOrigin: false }, '#giftCardIframe34070');
+      setLoading(false);
     };
     document.body.appendChild(script);
 
@@ -21,12 +24,14 @@ function GiftCard() {
       <h1>Lahjakortit</h1>
       <p>Anna lahjaksi hyvinvointia! </p>
       <div className="giftCardInfo">
-      <iframe
-        width="100%"
-        frameBorder="0"
-        src="https://varaa.timma.fi/giftcard/hierojaikolaelsa"
-        id="giftCardIframe34070"
-      ></iframe>
+        {loading && <p>Lahjakortti järjestelmän lataus on kesken. Odottakaa hetki.</p>}
+        <iframe
+          width="100%"
+          frameBorder="0"
+          src="https://varaa.timma.fi/giftcard/hierojaikolaelsa"
+          id="giftCardIframe34070"
+          onLoad={() => setLoading(false)}
+        ></iframe>
       </div>
     </div>
   );
