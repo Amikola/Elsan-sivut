cc929200f511ccacc8f96d657353b3bdc84bacdc
Atte <atte.ikola@gmail.com>
2025-01-02 12:13:26 +0200

    Added arrival guide

--- src/pages/ArrivalGuide/ArrivalGuide.tsx
+++ src/pages/ArrivalGuide/ArrivalGuide.tsx
@@ -3,7 +3,19 @@ import "./ArrivalGuide.css"
 function ArrivalGuide() {
     return (
       <div>
-        <h1>ArrivalGuide</h1>
+        <div className="ArrivalContent">
+        <h1>Saapumisohjeet</h1>
+        <div className="ArrivalText">
+        <p>Vastaanotto sijaitsee osoitteessa </p> <a href="https://www.google.com/maps?q=Arolantie+1+As+57+P%C3%B6yt%C3%A4alho,+04410+J%C3%A4rvenp%C3%A4%C3%A4"> Arolantie 1, 04410 Järvenpää.</a> 
+        <p> Ovi sijaitsee kartanonpolun puolella. Odotathan ulkona niin tulen avaamaan oven muutamaa minuuttia ennen aikaasi.</p>
+        </div>
+        <p className="ParkingText">Autolla 🚗 tultaessa voit pysäköidä tienvarteen Arolantielle, Kartanontielle tai Kartanonpolulle. </p>
+        </div>
+
+        <div className="ArrivalImages">
+        <img src="src/assets/ReittiElsa.png"></img>
+        <img src="src/assets/ElsaOvi.png"></img>
+        </div>
       </div>
     );
   }
