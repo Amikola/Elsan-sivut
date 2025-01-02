825f1a9a9a4751b112f96afd587ce49801e0cfba
Atte <atte.ikola@gmail.com>
2025-01-02 13:49:54 +0200

    Fixed issue with services

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -3,6 +3,10 @@ import './Services.css';
 import { NavLink } from "react-router-dom";
 
 function Services() {
+
+
+
+  
   return (
     <div>
       <div className="TitleServices">
@@ -10,29 +14,29 @@ function Services() {
       <p>Tarjolla eri pituisia hieronta-aikoja asiakkaan toiveiden mukaan. Valitsen tekniikat aina asiakkaan tarpeiden ja toiveiden perusteella. </p>
       </div>
       <div className="Services"> 
-        <div className="Classical">
+        <div className="ClassicalServices">
           <h2>Klassinen hieronta</h2>
           <p>Klassinen hieronta stressin ja jännitysten lievitykseen, sekä hyvinvoinnin työkaluna.</p>
           <ul>
-          <li><p>Klassinen hieronta</p> <p>30 €</p></li>
-          <li><p>Klassinen hieronta</p> <p>40 €</p></li>
-          <li><p>Klassinen hieronta</p> <p>50 €</p></li>
-          <li><p>Klassinen hieronta</p> <p>60 €</p></li>
-          <li><p>Klassinen hieronta</p> <p>70 €</p></li>
-          <li><p>Klassinen hieronta</p> <p>90 €</p></li>
+          <li><p>Klassinen hieronta (30 min)    </p> <p>30 €</p></li>
+          <li><p>Klassinen hieronta (45 min)    </p> <p>40 €</p></li>
+          <li><p>Klassinen hieronta (60 min)    </p> <p>50 €</p></li>
+          <li><p>Klassinen hieronta (75 min)    </p> <p>60 €</p></li>
+          <li><p>Klassinen hieronta (90 min)    </p> <p>70 €</p></li>
+          <li><p>Klassinen hieronta (120 min)   </p> <p>90 €</p></li>
           </ul>
           <NavLink to="/reservation" className="button1">Varaa aika!</NavLink>
         </div>
-        <div className="Sports">
+        <div className="SportsServices">
         <h2>Urheiluhieronta</h2>
         <p>Urheiluhieronta sopii kaikille aktiivisille ihmisille, kilpaurheilijoista tavalliseen arkiliikkujaan. Vastaanotolla voidaan käyttää liikelaajuuksia parantavia tekniikoita asiakkaan toiveden mukaan.</p>
         <ul>
-          <li><p>Urheilu hieronta</p> <p>30 €</p></li>
-          <li><p>Urheilu hieronta</p> <p>40 €</p></li>
-          <li><p>Urheilu hieronta</p> <p>50 €</p></li>
-          <li><p>Urheilu hieronta</p> <p>60 €</p></li>
-          <li><p>Urheilu hieronta</p> <p>70 €</p></li>
-          <li><p>Urheilu hieronta</p> <p>90 €</p></li>
+          <li><p>Urheilu hieronta (30 min) </p> <p>30 €</p></li>
+          <li><p>Urheilu hieronta (45 min) </p> <p>40 €</p></li>
+          <li><p>Urheilu hieronta (60 min) </p> <p>50 €</p></li>
+          <li><p>Urheilu hieronta (75 min) </p> <p>60 €</p></li>
+          <li><p>Urheilu hieronta (90 min) </p> <p>70 €</p></li>
+          <li><p>Urheilu hieronta (120 min) </p> <p>90 €</p></li>
           </ul>
           <NavLink to="/reservation" className="button2">Varaa aika!</NavLink>
         </div>
