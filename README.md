23a892d04fb1170f17032ac7a0dcd8bd5611669b
Atte <atte.ikola@gmail.com>
2025-08-21 22:35:43 +0300

    Added styling

--- README.md
+++ README.md
@@ -9,13 +9,16 @@ This website has been solo deveploment by mean and is not expected to be made pu
 Normal npm build process so : 
 
 npm i 
+
 npm run build
+
 npm run dev
+
 
 
 # Version control and deployment
 
-Version control is done using github using to branches dev and main(which is the production branch). Both which are hosted online.
+Version control is done using github using to branches dev and main (which is the production branch). Both which are hosted online.
 
 Deployment is done using Amazon Amplify and is deployd directly from github. 
 Sites can be seen at https://hierojaelsaikola.fi/ and https://dev.d36hbncpdtx8h2.amplifyapp.com/
--- src/pages/Reservation/Reservation.tsx
+++ src/pages/Reservation/Reservation.tsx
@@ -8,7 +8,6 @@ function Reservation() {
   const handleIframeLoad = () => {
     setIframeLoaded(true);
 
-    // Set iframe height and width to 100% of the viewport
     if (iframeRef.current) {
       iframeRef.current.style.height = `${window.innerHeight}px`;
       iframeRef.current.style.width = `${window.innerWidth}px`;
@@ -30,16 +29,37 @@ function Reservation() {
   return (
     <div className="reservation-container">
       <h1>Ajanvaraus</h1>
-      <p>Meillä maksutapana käy kortti, mobilepay sekä erilaiset hyvinvointi edut kuten E-passi, Smartum sekä Edenred.</p>
-      <div className="reservation-box">
-        {!iframeLoaded && <p>Ladataan ajanvaraus järjestelmää. Ole hyvä ja odota.</p>}
-        <iframe
-          ref={iframeRef}
-          width="50%"
-          height="100%"
-          src="https://www.varaaheti.fi/hoitokeidas/fi"
-          onLoad={handleIframeLoad}
-        ></iframe>
+      <div className="reservation-content-wrapper">
+        <div className="reservation-instructions-wrapper">
+          <div className="reservation-instructions">
+            <p><strong>Ajanvaraus hierontaan</strong></p>
+            <ol>
+              <li>Valitse palveluksi Hieronta</li>
+              <li>Valitse aika</li>
+              <li>Valitse työntekijä (Elsa)</li>
+              <li>Valitse päivämäärä</li>
+            </ol>
+          </div>
+          <div className="reservation-instructions">
+            <p><strong>Ajanvaraus osteopatiaan opiskelijatyönä</strong></p>
+            <ol>
+              <li>Valitse palveluksi Osteopatia</li>
+              <li>Valitse Osteopatia opiskelijatyönä</li>
+              <li>Valitse kesto</li>
+              <li>Valitse päivämäärä</li>
+            </ol>
+          </div>
+        </div>
+        <div className="reservation-box">
+          {!iframeLoaded && <p>Ladataan ajanvaraus järjestelmää. Ole hyvä ja odota.</p>}
+          <iframe
+            ref={iframeRef}
+            width="100%"
+            height="100%"
+            src="https://www.varaaheti.fi/hoitokeidas/fi"
+            onLoad={handleIframeLoad}
+          ></iframe>
+        </div>
       </div>
     </div>
   );
