e834b3949edc73c9e739f1292bbf65351331c62a
Atte <atte.ikola@gmail.com>
2025-08-21 23:11:24 +0300

    Update small issue with logo

--- src/App.tsx
+++ src/App.tsx
@@ -41,9 +41,9 @@ function App() {
             &#9776; {/* Unicode character for hamburger menu */}
           </div>
         )}
-        <a href="/">
-          <img src="assets/IMG_0418.PNG" alt="Logo" className="navbar-image" />
-        </a>
+        <NavLink to="/">
+          <img src="/assets/IMG_0418.PNG" alt="Logo" className="navbar-image" />
+        </NavLink>
         <ul className={isMobile && menuOpen ? 'show' : ''}>
           <li>
             <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')} onClick={closeMenu}>Etusivu</NavLink>
