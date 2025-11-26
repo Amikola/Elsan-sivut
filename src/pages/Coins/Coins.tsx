7eaf32ec843015de42cfd6ffd5c130bd2107832f
Atte <atte.ikola@gmail.com>
2025-11-27 00:16:58 +0200

    Set up connection to AWS ambda

--- src/pages/Coins/Coins.tsx
+++ src/pages/Coins/Coins.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+import API_URL from '../../../secrets.tsx';
+
 import './Coins.css';
 
+declare const process: any;
+
 function Coins() {
-    return (<div>Coins Page</div>);
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    
+
+    fetch(API_URL)
+      .then(res => res.json())              // parse JSON body
+      .then(data => setMessage(data.message || data)) // store in state
+
+  }, []);
+
+  return (
+    <div>
+      <h1>Coins Page</h1>
+      <p>{message}</p> {/* This will display Lambda response */}
+    </div>
+  );
 }
 
 export default Coins;
